4601e771036f39592713d21bc935c77b27ae4d85
Curlyathlete <malo.rueda@etu.unilim.fr>
2024-12-11 16:14:24 +0100

    Update formulaire-inscription.php

--- miniprojet204/formulaire-inscription.php
+++ miniprojet204/formulaire-inscription.php
@@ -1,53 +1,82 @@
 <!-- inscription.html -->
- <?php
+<?php
 
 
- include_once("inc.function.php");
- 
-     
-     
-     
+include_once("inc.function.php");
 
-     if($_POST && count($_POST)){
-	inscriptionprof();
 
-    }
- ?>
+
+
+
+if ($_POST && count($_POST)) {
+    inscriptionprof();
+
+}
+?>
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inscription</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
     <div class="container">
         <form action="" method="POST">
             <h2>Inscription</h2>
-            <label for="username_etudiant">Nom d'utilisateur</label>
-            <input type="text" id="username_etudiant" name="username_etudiant" required>
-            
-            <label for="password_etudiant">Mot de passe</label>
-            <input type="password_etudiant" id="password_etudiant" name="password_etudiant" required>
-            
+
+            <label for="num_etudiant">Numéro Étudiant</label>
+            <input type="text" id="num_etudiant" name="num_etudiant" required>
+
+            <label for="nom_etudiant">Nom</label>
+            <input type="text" id="nom_etudiant" name="nom_etudiant" required>
+
+            <label for="prenom_etudiant">Prénom</label>
+            <input type="text" id="prenom_etudiant" name="prenom_etudiant" required>
+
+            <label for="email_etudiant">E-mail</label>
+            <input type="email" id="email_etudiant" name="email_etudiant" required>
+
+            <label for="identifiant_etudiant">Identifiant</label>
+            <input type="text" id="identifiant_etudiant" name="identifiant_etudiant" required>
+
+            <label for="mot_de_passe_etudiant">Mot de passe</label>
+            <input type="password" id="mot_de_passe_etudiant" name="mot_de_passe_etudiant" required>
+
             <button type="submit">Soumettre</button>
         </form>
     </div>
     <div class="container">
         <form action="" method="POST">
-            <h2>Inscription</h2>
-            <label for="username_professeur">Nom d'utilisateur</label>
-            <input type="text" id="username_professeur" name="username_professeur" required>
-            
-            <label for="password_professeur">Mot de passe</label>
-            <input type="password_professeur" id="password_professeur" name="password_professeur" required>
-            
+            <h2>Inscription Professeur</h2>
+
+            <label for="id_prof">ID Professeur</label>
+            <input type="text" id="id_prof" name="id_prof" required>
+
+            <label for="nom_prof">Nom</label>
+            <input type="text" id="nom_prof" name="nom_prof" required>
+
+            <label for="prenom_prof">Prénom</label>
+            <input type="text" id="prenom_prof" name="prenom_prof" required>
+
+            <label for="email_prof">E-mail</label>
+            <input type="email" id="email_prof" name="email_prof" required>
+
+            <label for="identifiant_prof">Identifiant</label>
+            <input type="text" id="identifiant_prof" name="identifiant_prof" required>
+
+            <label for="mot_de_passe_prof">Mot de passe</label>
+            <input type="password" id="mot_de_passe_prof" name="mot_de_passe_prof" required>
+
             <button type="submit">Soumettre</button>
         </form>
     </div>
-<?php 
+    <?php
 
-?>
+    ?>
 </body>
+
 </html>
