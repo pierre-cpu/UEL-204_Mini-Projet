5ea7f9133e648b9823d276ae921cca3a5faca163
pierre-cpu <catala31@hotmail.fr>
2024-12-12 16:05:21 +0100

    Mise à jour de la base de donnée
    J'ai mofifier quelques unes des tables ou il y avait des erreurs ou champs inutiles

--- miniprojet204/BDD_universite.sql
+++ miniprojet204/BDD_universite.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : sam. 07 déc. 2024 à 15:30
+-- Généré le : jeu. 12 déc. 2024 à 15:04
 -- Version du serveur : 8.2.0
 -- Version de PHP : 8.2.13
 
@@ -18,171 +18,35 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `bdd_universite2`
+-- Base de données : `universite`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `cours`
+-- Structure de la table `notes`
 --
 
-DROP TABLE IF EXISTS `cours`;
-CREATE TABLE IF NOT EXISTS `cours` (
-  `code_uel` int NOT NULL,
-  `nom` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
-  `credits` int NOT NULL,
-  PRIMARY KEY (`code_uel`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
-
---
--- Déchargement des données de la table `cours`
---
-
-INSERT INTO `cours` (`code_uel`, `nom`, `credits`) VALUES
-(219, 'Web dynamique et bases de données', 6);
-
--- --------------------------------------------------------
-
---
--- Structure de la table `enseignements`
---
-
-DROP TABLE IF EXISTS `enseignements`;
-CREATE TABLE IF NOT EXISTS `enseignements` (
-  `id_prof` int DEFAULT NULL,
-  `code_uel` int DEFAULT NULL,
-  `debut` date NOT NULL,
-  `fin` date NOT NULL,
-  KEY `prof` (`id_prof`),
-  KEY `cours` (`code_uel`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `etudiants`
---
-
-DROP TABLE IF EXISTS `etudiants`;
-CREATE TABLE IF NOT EXISTS `etudiants` (
-  `id_etudiant` int NOT NULL AUTO_INCREMENT,
-  `numero_etudiant` int NOT NULL,
-  `nom` varchar(50) COLLATE utf8mb3_unicode_ci NOT NULL,
-  `prenom` varchar(50) COLLATE utf8mb3_unicode_ci NOT NULL,
-  `email` varchar(320) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
-  `identifiant` varchar(20) COLLATE utf8mb3_unicode_ci NOT NULL,
-  `motdepasse` varchar(255) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
-  PRIMARY KEY (`id_etudiant`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
-
---
--- Déchargement des données de la table `etudiants`
---
-
-INSERT INTO `etudiants` (`id_etudiant`, `numero_etudiant`, `nom`, `prenom`, `email`, `identifiant`, `motdepasse`) VALUES
-(1, 17932048, '', '', '', '', 'drowssap');
-
--- --------------------------------------------------------
-
---
--- Structure de la table `evaluation`
---
-
-DROP TABLE IF EXISTS `evaluation`;
-CREATE TABLE IF NOT EXISTS `evaluation` (
-  `id_eval` int NOT NULL AUTO_INCREMENT,
-  `code_uel` int NOT NULL,
-  `type` varchar(30) NOT NULL,
-  `intitule` varchar(30) NOT NULL,
-  `coeficient` int NOT NULL,
-  `date` date NOT NULL,
-  PRIMARY KEY (`id_eval`),
-  KEY `cours` (`code_uel`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `inscription`
---
-
-DROP TABLE IF EXISTS `inscription`;
-CREATE TABLE IF NOT EXISTS `inscription` (
-  `id_inscription` int NOT NULL AUTO_INCREMENT,
-  `id_etudiant` int DEFAULT NULL,
-  `code_uel` int DEFAULT NULL,
-  `date` timestamp NOT NULL,
-  PRIMARY KEY (`id_inscription`),
-  KEY `etudiant` (`id_etudiant`),
-  KEY `cours` (`code_uel`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `note`
---
-
-DROP TABLE IF EXISTS `note`;
-CREATE TABLE IF NOT EXISTS `note` (
-  `id_note` int NOT NULL AUTO_INCREMENT,
-  `id_etudiant` int DEFAULT NULL,
+DROP TABLE IF EXISTS `notes`;
+CREATE TABLE IF NOT EXISTS `notes` (
+  `numero_etudiant` char(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `id_eval` int DEFAULT NULL,
   `note` int NOT NULL,
-  `commentaire` text NOT NULL,
-  PRIMARY KEY (`id_note`),
-  KEY `etudiant` (`id_etudiant`),
-  KEY `evaluation` (`id_eval`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `professeurs`
---
-
-DROP TABLE IF EXISTS `professeurs`;
-CREATE TABLE IF NOT EXISTS `professeurs` (
-  `id_prof` int NOT NULL AUTO_INCREMENT,
-  `nom` varchar(50) NOT NULL,
-  `prenom` varchar(50) NOT NULL,
-  `email` varchar(320) NOT NULL,
-  `identifiant` varchar(20) NOT NULL,
-  `motdepasse` varchar(20) NOT NULL,
-  PRIMARY KEY (`id_prof`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `commentaire` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  KEY `evaluation` (`id_eval`),
+  KEY `etudiant` (`numero_etudiant`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Contraintes pour les tables déchargées
 --
 
 --
--- Contraintes pour la table `enseignements`
+-- Contraintes pour la table `notes`
 --
-ALTER TABLE `enseignements`
-  ADD CONSTRAINT `enseignements_ibfk_1` FOREIGN KEY (`id_prof`) REFERENCES `professeurs` (`id_prof`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `enseignements_ibfk_2` FOREIGN KEY (`code_uel`) REFERENCES `cours` (`code_uel`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Contraintes pour la table `evaluation`
---
-ALTER TABLE `evaluation`
-  ADD CONSTRAINT `evaluation_ibfk_1` FOREIGN KEY (`code_uel`) REFERENCES `cours` (`code_uel`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Contraintes pour la table `inscription`
---
-ALTER TABLE `inscription`
-  ADD CONSTRAINT `inscription_ibfk_1` FOREIGN KEY (`id_etudiant`) REFERENCES `etudiants` (`id_etudiant`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `inscription_ibfk_2` FOREIGN KEY (`code_uel`) REFERENCES `cours` (`code_uel`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Contraintes pour la table `note`
---
-ALTER TABLE `note`
-  ADD CONSTRAINT `note_ibfk_1` FOREIGN KEY (`id_etudiant`) REFERENCES `etudiants` (`id_etudiant`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `note_ibfk_2` FOREIGN KEY (`id_eval`) REFERENCES `evaluation` (`id_eval`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `notes`
+  ADD CONSTRAINT `_etudiant` FOREIGN KEY (`numero_etudiant`) REFERENCES `etudiants` (`numero_etudiant`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `evaluation` FOREIGN KEY (`id_eval`) REFERENCES `evaluations` (`id_eval`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
