9d7ab60f127a999e9673e024d27ed81173df6b7d
Curlyathlete <malo.rueda@etu.unilim.fr>
2024-12-14 12:41:26 +0100

    modif formulaire creation

--- miniprojet204/page_ceation-compte.php
+++ miniprojet204/page_ceation-compte.php
@@ -21,9 +21,12 @@ if ($_POST && count($_POST)) {
 </head>
 
 <body>
-    <div class="container">
+<div class="container">
         <form action="" method="POST">
             <h2>Inscription</h2>
+
+            <label for="numero_etudiant">Numéro d'étudiant :</label>
+            <input type="text" id="numero_etudiant" name="numero_etudiant" required>
 
             <label for="nom_etudiant">Nom</label>
             <input type="text" id="nom_etudiant" name="nom_etudiant" required>
@@ -39,28 +42,6 @@ if ($_POST && count($_POST)) {
 
             <label for="mot_de_passe_etudiant">Mot de passe</label>
             <input type="password" id="mot_de_passe_etudiant" name="mot_de_passe_etudiant" required>
-
-            <button type="submit">Soumettre</button>
-        </form>
-    </div>
-    <div class="container">
-        <form action="" method="POST">
-            <h2>Inscription Professeur</h2>
-
-            <label for="nom_prof">Nom</label>
-            <input type="text" id="nom_prof" name="nom_prof" required>
-
-            <label for="prenom_prof">Prénom</label>
-            <input type="text" id="prenom_prof" name="prenom_prof" required>
-
-            <label for="email_prof">E-mail</label>
-            <input type="email" id="email_prof" name="email_prof" required>
-
-            <label for="identifiant_prof">Identifiant</label>
-            <input type="text" id="identifiant_prof" name="identifiant_prof" required>
-
-            <label for="mot_de_passe_prof">Mot de passe</label>
-            <input type="password" id="mot_de_passe_prof" name="mot_de_passe_prof" required>
 
             <button type="submit">Soumettre</button>
         </form>
